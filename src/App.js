01aeb0eaad435701907099b0b1cc36abf81f2872
Vaibhavdesai99 <vaibhavdesai510@gmail.com>
2023-05-22 10:15:16 +0530

    Loading the application(when data is coming show loading..msg)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ function App() {
   // State to store the fetched movies
   const [movies, setMovies] = useState([]);
 
+  // To show movies are on its way to show on screen .. Loding...
+  const [showloader, setShowLoader] = useState(false);
+
   // Function to fetch movies data from the API
   async function fetchMovieHandler() {
     try {
+      setShowLoader(true);
       const response = await fetch("https://swapi.dev/api/films/");
 
       // Parse the JSON response
@@ -24,6 +28,7 @@ function App() {
 
       // Update the state with the transformed movies data
       setMovies(transformedMovies);
+      setShowLoader(false);
     } catch (error) {
       console.error("SOMETHING WENT WRONG :", error);
     }
@@ -35,7 +40,14 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {/* if not showloader then display the movies on screen */}
+
+        {!showloader && movies.length > 0 && <MoviesList movies={movies} />}
+
+        {!showloader && movies.length === 0 && <p>Found No Movies</p>}
+
+        {/* if there is loading time then display below msg. */}
+        {showloader && <p>Loading ...</p>}
       </section>
     </React.Fragment>
   );
